b0f57ded1bea6d89823a2f8220eef9554d792ce0
Vladislav Starokozhev <AgressoRj@gmail.com>
2018-10-18 13:28:45 +0300

    add Company entity

---
+++ src/main/java/org/communis/javawebintro/entity/Company.java
@@ -0,0 +1,40 @@
+package org.communis.javawebintro.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "company")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Company(String name){
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
